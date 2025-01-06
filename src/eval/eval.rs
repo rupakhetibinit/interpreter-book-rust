c8300bd6d6a19d60d3d12309429b0886e758886a
rupakhetibinit <crusierponder@protonmail.com>
2025-01-06 23:37:14 +0545

    Add support for basic infix and prefix expressions

--- src/eval/eval.rs
+++ src/eval/eval.rs
@@ -1,51 +1,99 @@
 use super::object::Object;
 use crate::ast::{Program, ast};
 
-pub fn eval(node: ast::Statement) -> Object {
+pub fn eval(node: ast::Statement) -> Option<Object> {
     match node {
-        ast::Statement::Let { token, name, value } => todo!(),
-        ast::Statement::Return { token, value } => todo!(),
-        ast::Statement::Block { token, statements } => todo!(),
+        ast::Statement::Let { token, name, value } => Some(todo!()),
+        ast::Statement::Return { token, value } => Some(todo!()),
+        ast::Statement::Block { token, statements } => Some(todo!()),
         ast::Statement::Expression(expression) => match expression {
-            ast::Expression::Integer { value, .. } => return Object::Int(value),
-            ast::Expression::Boolean { value, .. } => return Object::Bool(value),
+            ast::Expression::Integer { value, .. } => Some(Object::Int(value)),
+            ast::Expression::Boolean { value, .. } => Some(Object::Bool(value)),
             ast::Expression::Prefix {
-                token,
-                operator,
-                right,
-            } => todo!(),
+                right, operator, ..
+            } => {
+                let rt = eval(ast::Statement::Expression(right.unwrap()))?;
+                eval_prefix_expression(operator, rt)
+            }
             ast::Expression::Infix {
                 token,
                 operator,
                 right,
                 left,
-            } => todo!(),
+            } => {
+                let lt = eval(ast::Statement::Expression(*left))?;
+                let rt = eval(ast::Statement::Expression(*right))?;
+                eval_infix_expression(operator, lt, rt)
+            }
             ast::Expression::If {
                 token,
                 condition,
                 consequence,
                 alternative,
-            } => todo!(),
+            } => Some(todo!()),
             ast::Expression::Function {
                 token,
                 parameters,
                 body,
-            } => todo!(),
+            } => Some(todo!()),
             ast::Expression::Call {
                 token,
                 function,
                 arguments,
-            } => todo!(),
-            ast::Expression::Identifier(identifier) => todo!(),
-            ast::Expression::None => todo!(),
+            } => Some(todo!()),
+            ast::Expression::Identifier(identifier) => Some(todo!()),
+            ast::Expression::None => Some(todo!()),
         },
     }
 }
 
+fn eval_infix_expression(operator: &str, lt: Object, rt: Object) -> Option<Object> {
+    match (lt, rt) {
+        (Object::Int(l), Object::Int(r)) => eval_integer_infix_operation(operator, l, r),
+        _ => None,
+    }
+}
+
+fn eval_integer_infix_operation(operator: &str, l: i64, r: i64) -> Option<Object> {
+    match operator {
+        "+" => Some(Object::Int(l + r)),
+        "-" => Some(Object::Int(l - r)),
+        "*" => Some(Object::Int(l * r)),
+        "/" => Some(Object::Int(l / r)),
+        _ => None,
+    }
+}
+
 pub fn eval_program(program: &mut Program) -> Object {
-    let mut result: Object = Object::Null;
+    let mut result = Some(Object::Null);
     for stmt in program.statements.iter() {
         result = eval(stmt.clone())
     }
-    result
+    match result {
+        Some(x) => x,
+        None => Object::Null,
+    }
+}
+
+pub fn eval_prefix_expression(operator: &str, right: Object) -> Option<Object> {
+    match operator {
+        "-" => eval_minus_prefix_operator_expression(right),
+        "!" => Some(eval_bang_operator_expression(right)),
+        _ => None,
+    }
+}
+
+fn eval_minus_prefix_operator_expression(right: Object) -> Option<Object> {
+    match right {
+        Object::Int(i) => Some(Object::Int(-i)),
+        _ => None,
+    }
+}
+
+fn eval_bang_operator_expression(right: Object) -> Object {
+    match right {
+        Object::Bool(r) => Object::Bool(!r),
+        Object::Int(_) => Object::Bool(false),
+        Object::Null => Object::Bool(true),
+    }
 }
--- src/eval/tests.rs
+++ src/eval/tests.rs
@@ -20,3 +20,102 @@ pub fn test_int_eval() {
         assert_eq!(format!("{}", actual), format!("{}", expected))
     }
 }
+
+#[test]
+pub fn test_bool_eval() {
+    let input = ["true;", "false"];
+    let expected = [true, false];
+
+    for (input, expected) in input.iter().zip(expected) {
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+
+        let program = parser.parse_program();
+
+        assert!(program.is_some());
+        assert!(parser.errors.is_empty());
+
+        let mut program = program.unwrap();
+        let actual = eval_program(&mut program);
+
+        assert_eq!(format!("{}", actual), format!("{}", expected))
+    }
+}
+
+#[test]
+pub fn test_prefix_operator_eval() {
+    let input = ["!true", "!false", "!5", "!!true", "!!false", "!!5"];
+    let expected = [false, true, false, true, false, true];
+
+    for (input, expected) in input.iter().zip(expected) {
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+
+        let program = parser.parse_program();
+
+        assert!(program.is_some());
+        assert!(parser.errors.is_empty());
+
+        let mut program = program.unwrap();
+        let actual = eval_program(&mut program);
+
+        assert_eq!(format!("{}", actual), format!("{}", expected))
+    }
+}
+
+#[test]
+pub fn test_minus_operator() {
+    let input = ["-1", "-14", "-12341234", "12341234", "-124234", "1234124"];
+    let expected = [-1, -14, -12341234, 12341234, -124234, 1234124];
+
+    for (input, expected) in input.iter().zip(expected) {
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+
+        let program = parser.parse_program();
+
+        assert!(program.is_some());
+        assert!(parser.errors.is_empty());
+
+        let mut program = program.unwrap();
+        let actual = eval_program(&mut program);
+
+        assert_eq!(format!("{}", actual), format!("{}", expected))
+    }
+}
+
+#[test]
+pub fn test_infix_operators() {
+    let inputs_expected = [
+        ("5", 5),
+        ("10", 10),
+        ("-5", -5),
+        ("-10", -10),
+        ("5 + 5 + 5 + 5 - 10)", 10),
+        ("2 * 2 * 2 * 2 * 2", 32),
+        (" - 50 + 100 + -50", 0),
+        ("5 * 2 + 10", 20),
+        ("5 + 2 * 10", 25),
+        ("20 + 2 * -10", 0),
+        ("50 / 2 * 2 + 10", 60),
+        ("2 * (5 + 10)", 30),
+        ("3 * 3 * 3 + 10", 37),
+        ("3 * (3 * 3) + 10", 37),
+        ("(5 + 10 * 2 + 15 / 3) * 2 + -10", 50),
+    ];
+
+    for (input, expected) in inputs_expected {
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+
+        let program = parser.parse_program();
+
+        assert!(program.is_some());
+        assert!(parser.errors.is_empty());
+
+        let mut program = program.unwrap();
+        let actual = eval_program(&mut program);
+
+        assert_eq!(format!("{}", actual), format!("{}", expected))
+    }
+}
